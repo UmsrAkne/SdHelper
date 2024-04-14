5f3a3b637e04bdc15c6fac9a1665c6922e2785c2
Akn <sibata486@gmail.com>
2024-04-14 13:11:15 +0900

    init

---
+++ SdHelper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace SdHelper.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
