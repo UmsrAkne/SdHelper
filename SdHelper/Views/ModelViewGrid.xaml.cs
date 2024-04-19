659ddbc46cb0d7e6016bf56e4683558f3f5635c5
Akn <sibata486@gmail.com>
2024-04-19 22:19:09 +0900

    add / Model の情報を表示するユーザーコントロールを追加

---
+++ SdHelper/Views/ModelViewGrid.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SdHelper.Views
+{
+    public partial class ModelViewGrid : UserControl
+    {
+        public ModelViewGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
