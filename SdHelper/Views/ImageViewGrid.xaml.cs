c453fb52141c993739243e8392a62fb57ed5eb55
Akn <sibata486@gmail.com>
2024-04-19 22:42:48 +0900

    add / 画像ビュー画面を追加

---
+++ SdHelper/Views/ImageViewGrid.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SdHelper.Views
+{
+    public partial class ImageViewGrid : UserControl
+    {
+        public ImageViewGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
