0d942aa2780359f1cbfdc94f60e40522674e4a47
Akn <sibata486@gmail.com>
2024-04-19 22:23:57 +0900

    replace / Page を使用していた箇所を UserControl に変更
    Page (というか Frame) に親のデータコンテキストを設定する方法がわからなかったため、
    Page から UserControl を使用。

--- SdHelper/Views/Behaviors/FileDropBehavior.cs
+++ SdHelper/Views/Behaviors/FileDropBehavior.cs
@@ -49,10 +49,10 @@ namespace SdHelper.Views.Behaviors
                 {
                     // ReSharper disable once StringLiteralTypo
                     case ".safetensors":
-                        ((MainWindowViewModel)listView.DataContext).ModelViewPageViewModel.ModelFileInfos.Add(new FileInfoWrapper(new FileInfo(f)));
+                        ((ModelViewPageViewModel)listView.DataContext).ModelFileInfos.Add(new FileInfoWrapper(new FileInfo(f)));
                         break;
                     case ".png":
-                        ((MainWindowViewModel)listView.DataContext).ModelViewPageViewModel.ReplacePreviewImage(fileInfo.FullName);
+                        ((ModelViewPageViewModel)listView.DataContext).ReplacePreviewImage(fileInfo.FullName);
                         return;
                 }
             }
