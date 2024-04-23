54ba97a5e2a245f48ed79957b7d19969d6b0f709
Akn <sibata486@gmail.com>
2024-04-23 21:52:15 +0900

    improve / 画像ファイルのプレビューをビューに表示

--- SdHelper/ViewModels/ImageViewGridViewModel.cs
+++ SdHelper/ViewModels/ImageViewGridViewModel.cs
@@ -7,6 +7,14 @@ namespace SdHelper.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class ImageViewGridViewModel : BindableBase
     {
+        private FileInfoWrapper selectedImageFile;
+
         public ObservableCollection<FileInfoWrapper> ImageFiles { get; set; } = new ();
+
+        public FileInfoWrapper SelectedImageFile
+        {
+            get => selectedImageFile;
+            set => SetProperty(ref selectedImageFile, value);
+        }
     }
 }
