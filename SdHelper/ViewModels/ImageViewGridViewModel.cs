afc057d7d96588197c025ea8760bb9c4044bb9c5
Akn <sibata486@gmail.com>
2024-04-23 21:37:47 +0900

    improve / 画像ファイルをイメージビュー画面に DD できるようにした

--- SdHelper/ViewModels/ImageViewGridViewModel.cs
+++ SdHelper/ViewModels/ImageViewGridViewModel.cs
@@ -1,9 +1,12 @@
+using System.Collections.ObjectModel;
 using Prism.Mvvm;
+using SdHelper.Models;
 
 namespace SdHelper.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class ImageViewGridViewModel : BindableBase
     {
+        public ObservableCollection<FileInfoWrapper> ImageFiles { get; set; } = new ();
     }
 }
