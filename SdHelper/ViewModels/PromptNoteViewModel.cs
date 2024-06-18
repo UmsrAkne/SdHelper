527b47209fd00b4e0e12c0e71d32f61eaa4d0165
Akn <sibata486@gmail.com>
2024-06-18 22:21:13 +0900

    add / Prompt のメモ（ダミー）が PromptNote に表示されるところまで処理を実装。

--- SdHelper/ViewModels/PromptNoteViewModel.cs
+++ SdHelper/ViewModels/PromptNoteViewModel.cs
@@ -1,9 +1,40 @@
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Prism.Mvvm;
+using SdHelper.Models;
 
 namespace SdHelper.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class PromptNoteViewModel : BindableBase
     {
+        private ObservableCollection<ImageGenerationPrompt> imageGenerationPrompts = new ();
+
+        public PromptNoteViewModel()
+        {
+            InjectDummies(); // デバッグモード時に限り実行される
+        }
+
+        public ObservableCollection<ImageGenerationPrompt> ImageGenerationPrompts
+        {
+            get => imageGenerationPrompts;
+            private set => SetProperty(ref imageGenerationPrompts, value);
+        }
+
+        [Conditional("DEBUG")]
+        private void InjectDummies()
+        {
+            for (var i = 0; i < 30; i++)
+            {
+                var p = new ImageGenerationPrompt()
+                {
+                    Name = new TextWrapper() { Text = $"testName{i}", },
+                    Prompt = new TextWrapper() { Text = $"best quality, test prompt, ((no {i})), (test), text", },
+                    NegativePrompt = new TextWrapper() { Text = $"low quality, no {i}", },
+                };
+
+                ImageGenerationPrompts.Add(p);
+            }
+        }
     }
 }
